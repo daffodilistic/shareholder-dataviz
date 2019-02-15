b1d121f2429dd6493949e1b1966a200d6febfde2
Soh Thiam Hing <daffodilistic@gmail.com>
2019-02-15 17:06:01 +0800

    Basic heatmap generation done with randomly generated data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -32,7 +32,9 @@ export default {
   },
   data() {
     return {
-      data: null
+      geocodeData: null,
+      data: null,
+      mapRef: null
     }
   },
   async mounted() {
@@ -41,7 +43,9 @@ export default {
       const geocoder = new google.maps.Geocoder();
       const map = new google.maps.Map(document.getElementById('map'));
 
-      geocoder.geocode({ address: 'Singapore 600332' }, (results, status) => {
+      this.mapRef = map;
+
+      geocoder.geocode({ address: 'Singapore' }, (results, status) => {
         if (status !== 'OK' || !results[0]) {
           throw new Error(status);
         }
@@ -52,8 +56,53 @@ export default {
     } catch (error) {
       console.error(error);
     }
+
+    const axios = require('axios');
+
+    // Make a request for a user with a given ID
+    await axios.get('https://raw.githubusercontent.com/xkjyeah/singapore-postal-codes/master/buildings.json')
+      .then((response) => {
+        // handle success
+        // console.log(response.data);
+        this.geocodeData = response.data;
+        this.generateData();
+
+        var heatmap = new google.maps.visualization.HeatmapLayer({
+          data: this.data,
+          map: this.mapRef
+        });
+        // heatmap.setMap(heatmap.getMap() ? null : this.mapRef);
+
+        // console.log("mapRef is " + this.mapRef);
+
+        // heatmap.setMap(this.mapRef);
+
+        // delete this.geocodeData;
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      })
   },
   methods: {
+    generateData() {
+      var userPostalCode = [];
+      var users = 1500;
+
+      for (var i = 1; i <= users; i++) {
+        var index = this.getRandomIntInclusive(0,this.geocodeData.length);
+        var gpsCoord = new google.maps.LatLng(
+          this.geocodeData[index].LATITUDE,
+          this.geocodeData[index].LONGITUDE
+        );
+
+        // console.log(this.geocodeData[index].LATITUDE + ", " + this.geocodeData[index].LONGITUDE);
+        userPostalCode.push(gpsCoord);
+      }
+
+      this.data = userPostalCode;
+      // console.log(this.data);
+    },
     onFileChanged() {
       var file = event.target.files[0];
       const reader = new FileReader();
@@ -66,8 +115,14 @@ export default {
         this.data = data;
 
         console.log(this.data);
+        
       };
       reader.readAsBinaryString(file);
+    },
+    getRandomIntInclusive(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
     }
   }
 };
--- src/utils/gmaps.js
+++ src/utils/gmaps.js
@@ -31,7 +31,7 @@ export default function init() {
   const script = document.createElement('script');
   script.async = true;
   script.defer = true;
-  script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=${CALLBACK_NAME}`;
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=${CALLBACK_NAME}&libraries=visualization`;
   script.onerror = rejectInitPromise;
   document.querySelector('head').appendChild(script);
 
